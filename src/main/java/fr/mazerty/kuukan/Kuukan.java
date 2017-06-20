9b5b242ddaac53006248997932b0a0db275e0e51
mazerty <mazerty@gmail.com>
2017-06-20 07:43:42 +0200

    empty jmonkeyengine application

---
+++ src/main/java/fr/mazerty/kuukan/Kuukan.java
@@ -0,0 +1,15 @@
+package fr.mazerty.kuukan;
+
+import com.jme3.app.SimpleApplication;
+
+public class Kuukan extends SimpleApplication {
+
+    public static void main(String[] args) {
+        new Kuukan().start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+    }
+
+}
