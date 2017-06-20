668b65dde8d1e668824c2faa6b7c2963691ddbf8
mazerty <mazerty@gmail.com>
2017-06-20 13:50:47 +0200

    pseudorandom cubic galaxy

--- src/main/java/fr/mazerty/kuukan/Kuukan.java
+++ src/main/java/fr/mazerty/kuukan/Kuukan.java
@@ -8,8 +8,13 @@ import com.jme3.post.FilterPostProcessor;
 import com.jme3.post.filters.BloomFilter;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Sphere;
+import org.apache.commons.math3.random.MersenneTwister;
+
+import java.util.stream.IntStream;
 
 public class Kuukan extends SimpleApplication {
+
+    private final MersenneTwister mt = new MersenneTwister(42);
 
     public static void main(String[] args) {
         new Kuukan().start();
@@ -17,19 +22,21 @@ public class Kuukan extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Sphere sphere = new Sphere(20, 20, 1);
+        Sphere sphere = new Sphere(3, 4, 0.01f);
 
         Material material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         material.setColor("Color", ColorRGBA.White);
         material.setColor("GlowColor", ColorRGBA.White);
 
-        Geometry geometry = new Geometry("sphere", sphere);
-        geometry.setMaterial(material);
-
-        rootNode.attachChild(geometry);
+        IntStream.range(0, 1000).forEach(i -> {
+            Geometry geometry = new Geometry("sphere" + i, sphere);
+            geometry.setMaterial(material);
+            geometry.setLocalTranslation(mt.nextFloat(), mt.nextFloat(), mt.nextFloat());
+            rootNode.attachChild(geometry);
+        });
 
         BitmapText text = new BitmapText(guiFont);
-        text.setText("Glowing star");
+        text.setText("Pseudorandom cubic galaxy");
         text.setLocalTranslation(0, settings.getHeight(), 0);
 
         guiNode.attachChild(text);
