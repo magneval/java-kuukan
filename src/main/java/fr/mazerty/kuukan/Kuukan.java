76b701230cce20c2d9fc85ceeafdbbd590802562
mazerty <mazerty@gmail.com>
2017-06-20 07:54:18 +0200

    a simple box

--- src/main/java/fr/mazerty/kuukan/Kuukan.java
+++ src/main/java/fr/mazerty/kuukan/Kuukan.java
@@ -1,6 +1,9 @@
 package fr.mazerty.kuukan;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
 
 public class Kuukan extends SimpleApplication {
 
@@ -10,6 +13,14 @@ public class Kuukan extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
+        Box box = new Box(1, 1, 1);
+
+        Material material = new Material(assetManager, "Common/MatDefs/Misc/ShowNormals.j3md");
+
+        Geometry geometry = new Geometry("box", box);
+        geometry.setMaterial(material);
+
+        rootNode.attachChild(geometry);
     }
 
 }
